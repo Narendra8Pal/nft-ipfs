1e357a83e59dee3ca6631141c8f48b84cba0999c
Narendra Pal <narenderpal.singhnsr@gmail.com>
2023-04-21 01:21:48 -0700

    lasst change sorry for so many commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -80,7 +80,7 @@ export default function Home() {
 
 
 // the changes you make in git source control can be pushed on git hub using terminal??
-
+// what's the use of git pull origin master
 
 	// and now making this change in vi of bash
 // will be deleting all these changes and commits 
